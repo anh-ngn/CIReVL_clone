6f8c9ef55405c36fdcdc982363ae3870b6ebe923
Anh Nguyen <anh.ng.rx@gmail.com>
2024-05-18 16:13:53 +0700

    skip the loop

--- src/utils.py
+++ src/utils.py
@@ -56,6 +56,7 @@ def extract_image_features(device: torch.device, args: argparse.Namespace, datas
         print("start extracting features")
         for batch in tqdm.tqdm(loader):
             print("Hello world")
+            continue
             if 'genecis' in args.dataset:
                 _, n_gallery, _, h, w = batch[3].size()
                 images = batch[3].view(-1, 3, h, w)
